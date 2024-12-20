95517e7fdd88fdccf5621aa06ad5623a4f426641
Jaikishore <jaikishore@dlearners.in>
2024-12-20 18:26:04 +0530

    3rd slide rework inprogress

---
+++ Assets/Script/RunningClouds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningClouds : MonoBehaviour
+{
+    public Transform spawnPoint, endPoint;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/Thumbnail2Controller.cs
+++ Assets/Script/Thumbnail2Controller.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Thumbnail2Controller : MonoBehaviour
 {
     public GameObject[] rainbowPoints;
+    public float rotateSpeed = 0f;
+    public float minAngle, maxAngle;
+    public  GameObject cardObj;
     bool canLog = true;
 
     void Start()
     {
-        
+        Debug.Log($"UP :: {Vector3.up}");
+        Debug.Log($"RIGHT :: {Vector3.right}");
+        Debug.Log($"Forward :: {Vector3.forward}");
     }
 
     private void OnEnable() {
@@ -27,25 +32,36 @@ public class Thumbnail2Controller : MonoBehaviour
 
     void OnCardDragged(GameObject dragObj)
     {
-        if(!canLog) return;
 
         List<float> distance = new List<float>();
         for (int i = 0; i < rainbowPoints.Length; i++)
         {
             distance.Add(Vector3.Distance(rainbowPoints[i].transform.position, dragObj.transform.position));
         }
-        Debug.Log($"OLD first object {distance[0]}");
-        List<float> _distance = new List<float>(distance);
-        // List<float> _distance = distance;
-        _distance.Sort();
-        Debug.Log($"NEW first object {_distance[0]}");
-        // distance.FindIndex(0, 1, _distance[0])
-        // foreach (var item in distance)
-        // {
-        //     Debug.Log(item);
-        // }
-        // canLog = false;
 
-        Debug.Log("-----------------------");
+        List<float> _distance = new List<float>(distance);
+        _distance.Sort();
+        int nearObjectIndex = distance.IndexOf(_distance[0]);
+
+        // dragObj.transform.LookAt(rainbowPoints[nearObjectIndex].transform, Vector3.right);
+
+        Vector3 lookDir = (rainbowPoints[nearObjectIndex].transform.position - dragObj.transform.position).normalized;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+
+        angle = Mathf.Clamp(angle, minAngle, maxAngle);
+
+        Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
+        Debug.Log(angle);
+
+        // lookDir.y = 0f;
+        // var __rotation = Quaternion.LookRotation(lookDir);
+        dragObj.transform.rotation = Quaternion.Slerp(dragObj.transform.rotation, targetRotation, rotateSpeed);
+
+        // float angle = Mathf.Atan2(rainbowPoints[nearObjectIndex].transform.position.y - dragObj.transform.position.y, rainbowPoints[nearObjectIndex].transform.position.x - dragObj.transform.position.x ) * Mathf.Rad2Deg;
+        // Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        // dragObj.transform.rotation = Quaternion.RotateTowards(dragObj.transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+
+        // var cardfaceDirection = Vector3.RotateTowards(dragObj.transform.position, rainbowPoints[nearObjectIndex].transform.position, rotateSpeed, 0.0f);
+        // dragObj.transform.rotation = Quaternion.LookRotation(cardfaceDirection);
     }
 }
