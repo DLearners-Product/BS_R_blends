b51e78711c696afba827ffb1738a12866f907ee0
Jaikishore <jaikishore@dlearners.in>
2024-12-27 14:07:39 +0530

    6th slide inprogress

--- Assets/Script/Thumbnail6Controller.cs
+++ Assets/Script/Thumbnail6Controller.cs
@@ -6,25 +6,23 @@ using UnityEngine.UI;
 
 public class Thumbnail6Controller : MonoBehaviour
 {
-    public Image IMG_questionImage;
+    public GameObject IMG_questionImagePanel;
     public GameObject G_optionsParent;
-    public static Thumbnail6Controller instance;
-    delegate void DummyDelegate();
-    DummyDelegate dummyDelegate;
+    public Transform T_startPoint, T_endPoint;
+    public Sprite[] SPR_questionSprite;
+    public GameObject G_questionImagePrefab;
+    GameObject _currentQuestion = null;
+    GameObject _prevQuestion = null;
+    int _currentIndex = 0;
 
     void Start()
     {
-        if(instance != null) instance = this;
         OpenOptions();
-        // AppendToDummyDelegate();
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            dummyDelegate?.Invoke();
-        }
+
     }
 
     void OpenOptions()
@@ -39,6 +37,21 @@ public class Thumbnail6Controller : MonoBehaviour
                 }
             );
         }
+        SpawnQuestion();
+    }
+
+    void CloseOptions()
+    {
+        int childCount = G_optionsParent.transform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform _obj = G_optionsParent.transform.GetChild(i);
+            Utilities.Instance.ANIM_RotateObjWithCallback(_obj, 
+                () => {
+                    Thumbnail6Controller.EnableOptionBG(_obj.gameObject);
+                }
+            );
+        }
     }
 
     public static void EnableOptionText(GameObject optionObj)
@@ -47,18 +60,34 @@ public class Thumbnail6Controller : MonoBehaviour
         optionObj.transform.GetChild(0).gameObject.SetActive(true);
     }
 
-    void AppendToDummyDelegate()
+    public static void EnableOptionBG(GameObject optionObj)
     {
-        int dummyVar = -1;
-        for (int i = 0; i < 5; i++)
-        {
-            dummyDelegate += () => { Logging((dummyVar + i).ToString()); };
-        }
-        dummyVar = 80;
+        optionObj.transform.GetChild(0).gameObject.SetActive(false);
+        optionObj.transform.GetChild(1).gameObject.SetActive(true);
     }
 
-    void Logging(string logMessage)
+    void SpawnQuestion()
     {
-        Debug.Log($"Log Message :: {logMessage}");
+        _prevQuestion = _currentQuestion;
+        MovePrevQuestion();
+        var spawnedQuestion = Instantiate(G_questionImagePrefab, IMG_questionImagePanel.transform);
+        spawnedQuestion.GetComponent<Image>().sprite = SPR_questionSprite[_currentIndex];
+        spawnedQuestion.GetComponent<Image>().preserveAspect = true;
+        spawnedQuestion.GetComponent<RectTransform>().offsetMin = Vector2.zero;
+        spawnedQuestion.GetComponent<RectTransform>().offsetMax = Vector2.zero;
+        _currentQuestion = spawnedQuestion;
+        Utilities.Instance.ANIM_Move(spawnedQuestion.transform, T_startPoint.position, 0f, MoveQuestion);
+    }
+
+    void MoveQuestion()
+    {
+        Utilities.Instance.ANIM_MoveWithScaleUp(_currentQuestion.transform, IMG_questionImagePanel.transform.position);
+    }
+
+    void MovePrevQuestion()
+    {
+        if(_prevQuestion == null) return;
+
+        Utilities.Instance.ANIM_Move(_prevQuestion.transform, T_endPoint.position);
     }
 }
