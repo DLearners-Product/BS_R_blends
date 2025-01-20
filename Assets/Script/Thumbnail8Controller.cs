1436f7387f1ddd999aa785d491b81086e5ed0455
Jaikishore <jaikishore@dlearners.in>
2025-01-20 18:30:37 +0530

    10th thumbnail inprogress

---
+++ Assets/Script/Thumbnail8Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thumbnail8Controller : MonoBehaviour
+{
+    public Transform textContainPanel;
+    public GameObject nextBtn, prevBtn;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnNextBtnClick()
+    {
+        if(textContainPanel.GetChild(1).gameObject.activeSelf) return;
+
+        Vector3 endPosition = textContainPanel.position + (Vector3.up * 10);
+        Utilities.Instance.ANIM_Move(textContainPanel, endPosition, callBack: () => { Enable2ndPanel(1); });
+    }
+
+    public void OnPrevBtnClick()
+    {
+        if(textContainPanel.GetChild(0).gameObject.activeSelf) return;
+
+        Vector3 endPosition = textContainPanel.position + (Vector3.up * 10);
+        Utilities.Instance.ANIM_Move(textContainPanel, endPosition, callBack: () => { Enable2ndPanel(2); });
+    }
+
+    void Enable2ndPanel(int enableIndex)
+    {
+        if(enableIndex == 1)
+        {
+            textContainPanel.GetChild(0).gameObject.SetActive(false);
+            textContainPanel.GetChild(1).gameObject.SetActive(true);
+            nextBtn.SetActive(false);
+            prevBtn.SetActive(true);
+        }else{
+            textContainPanel.GetChild(0).gameObject.SetActive(true);
+            textContainPanel.GetChild(1).gameObject.SetActive(false);
+            nextBtn.SetActive(true);
+            prevBtn.SetActive(false);
+        }
+
+        Vector3 endPosition = textContainPanel.position + (Vector3.down * 10);
+        Utilities.Instance.ANIM_Move(textContainPanel, endPosition);
+    }
+}
---
+++ Assets/Script/Thumbnail9Controller.cs
@@ -0,0 +1,91 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Thumbnail9Controller : MonoBehaviour
+{
+    public TextMeshProUGUI questionText;
+    public GameObject answerObject;
+    public string[] questionSTR;
+    public string[] answerSTR;
+    public GameObject[] optionObjects;
+    int index = 0;
+    Color intialColor;
+
+    void Start()
+    {
+        questionText.text = questionSTR[index];
+        intialColor = answerObject.GetComponent<Image>().color;
+        ShrinkOptionObjects();
+        SpawnOptions();
+    }
+
+    void OnEnable() {
+        ImageDragandDrop.onDragStart += OnObjectDragStart;
+        ImageDragandDrop.onDragEnd += OnObjectDragEnd;
+        ImageDropSlot.onDropInSlot += OnObjectDrop;
+    }
+
+    void OnObjectDrop(GameObject dropObj)
+    {
+        string selectedOptionSTR = dropObj.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text;
+        if(answerSTR[index] == selectedOptionSTR)
+        {
+            answerObject.GetComponent<Image>().color = Color.white;
+            answerObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = selectedOptionSTR;
+            Destroy(dropObj);
+            RightAnswer();
+        }
+    }
+
+    void OnObjectDragStart(GameObject dragObject)
+    {
+        Utilities.Instance.ScaleObject(answerObject.transform, scaleSize: 1.25f, duration: 0.5f);
+    }
+
+    void OnObjectDragEnd(GameObject dragObject)
+    {
+        Utilities.Instance.ScaleObject(answerObject.transform, scaleSize: 1f, duration: 0.5f);
+    }
+
+    void RightAnswer()
+    {
+        MoveAnswerPanel(Vector3.up * 1.25f);
+    }
+
+    void ChangeQuestion()
+    {
+        index++;
+        questionText.text = questionSTR[index];
+    }
+
+    void MoveAnswerPanel(Vector3 _position)
+    {
+        Vector3 endPos = answerObject.transform.position + _position;
+        Utilities.Instance.ANIM_Move(answerObject.transform, endPos, callBack: () => { MoveQuestionPanel(Vector3.up * 1.25f); });
+    }
+
+    void MoveQuestionPanel(Vector3 _position)
+    {
+        Vector3 endPos = questionText.transform.parent.position + _position;
+        Utilities.Instance.ANIM_Move(questionText.transform.parent, endPos, callBack: ChangeQuestion);
+    }
+
+    void ShrinkOptionObjects()
+    {
+        foreach (var opt in optionObjects)
+        {
+            Utilities.Instance.ANIM_ShrinkObject(opt.transform, 0f);
+        }
+    }
+
+    void SpawnOptions(int index = 0)
+    {
+        if(index >= optionObjects.Length) return;
+
+        Utilities.Instance.ANIM_ShowBounceNormal(optionObjects[index].transform, shrinkUpTime: 0.25f, callback: () => {
+            SpawnOptions(++index);
+        });
+    }
+
+}
